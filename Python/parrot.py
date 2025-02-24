e60a520ac92dbfa7e856f65f8ea0bddec9159b74
Niklas Rueber <nrueber@igus.net>
2025-02-24 17:18:06 +0100

    refactor: move compute base speed for voltage to Norwegian Blue Parrot
    Co-authored-by: Stefan Boos <kontakt@boos.systems>
    Co-authored-by: Holger Janßen-Kroll <hjanssenkroll@igus.net>
    Co-authored-by: Niklas Rueber <niklas@rueber.cloud>
    Co-authored-by: Bernhard Schenk <bernhardschenk@gmx.de>
    Co-authored-by: Michael Mahlberg <mm@consulting-guild.de>

--- Python/parrot.py
+++ Python/parrot.py
@@ -21,12 +21,13 @@ def parrot_factory(type_of_parrot, number_of_coconuts, voltage, nailed):
             return Parrot(type_of_parrot, number_of_coconuts, voltage, nailed)
 
 class Parrot:
-    minimal_voltage_speed = 24.0
+    # todo: have a look where these variables are needed and pull them there
     base_speed = 12.0
     base_load_factor = 9.0
 
 
     # todo: type_of_parrot indicates that a sub-type should be present - the class may have multiple responsibilities
+    # todo: we don't need all the parameters in constructor for all the parrots
     def __init__(self, type_of_parrot, number_of_coconuts, voltage, nailed):
         self._type = type_of_parrot
         self._number_of_coconuts = number_of_coconuts
@@ -39,9 +40,6 @@ class Parrot:
             
     def cry(self):
         raise NotImplementedError("This parrot cannot cry but it should")
-
-    def _compute_base_speed_for_voltage(self, voltage):
-        return min([Parrot.minimal_voltage_speed, voltage * Parrot.base_speed])
 
 
 class EuropeanParrot  (Parrot) :
@@ -69,6 +67,8 @@ class AfricanParrot  (Parrot) :
 
 
 class NorwegianBlueParrot(Parrot):
+    _maximum_voltage_speed = 24.0
+
     def __init__(self, number_of_coconuts, voltage, nailed):
         super().__init__(ParrotType.NORWEGIAN_BLUE, number_of_coconuts, voltage, nailed)
 
@@ -81,3 +81,6 @@ class NorwegianBlueParrot(Parrot):
         
     def speed(self):
         return 0 if self._nailed else self._compute_base_speed_for_voltage(self._voltage)
+
+    def _compute_base_speed_for_voltage(self, voltage):
+        return min([self._maximum_voltage_speed, voltage * Parrot.base_speed])
