b1b6348140eb1b5fd5520eeb2e5a4cd5c247784f
Stefan Boos <sboos@igus.net>
2025-03-06 16:10:27 +0000

    docs: document code smells

--- Python/parrot.py
+++ Python/parrot.py
@@ -10,18 +10,23 @@ class ParrotType(Enum):
 class Parrot:
 
     def __init__(self, type_of_parrot, number_of_coconuts, voltage, nailed):
+        # This class has three reasons for change (single responsibility violated)
+        # Some variables are only required by subtypes of a parrot
         self._type = type_of_parrot
         self._number_of_coconuts = number_of_coconuts
         self._voltage = voltage
         self._nailed = nailed
 
     def speed(self):
+        # Switch case is duplicate - makes it hard to add more parrot types
+        # Switch case executes specific logic for individual types of parrots
         match self._type:
             case ParrotType.EUROPEAN:
                 return self._base_speed()
             case ParrotType.AFRICAN:
                 return max(0, self._base_speed() - self._load_factor() * self._number_of_coconuts)
             case ParrotType.NORWEGIAN_BLUE:
+                # We dislike the single line if .. else .. construct
                 return 0 if self._nailed else self._compute_base_speed_for_voltage(self._voltage)
 
     def cry(self):
@@ -31,11 +36,14 @@ class Parrot:
             case ParrotType.AFRICAN:
                 return "Sqaark!"
             case ParrotType.NORWEGIAN_BLUE:
+                # We dislike the single line if .. else .. construct
                 return "Bzzzzzz" if self._voltage > 0 else "..."
 
+    # This method is only required for the NORWEGIAN_BLUE parrot
     def _compute_base_speed_for_voltage(self, voltage):
         return min([24.0, voltage * self._base_speed()])
 
+    # This method is only required for the AFRICAN parrot
     def _load_factor(self):
         return 9.0
 
